269962bcbfc16b2369d64e66941b8eec0c61c07b
hasancemreok <hasancemreok@hotmail.com>
2018-05-30 21:58:06 +0300

    Add project files.

---
+++ speedtest_implementation_for_net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speedtest_implementation_for_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
